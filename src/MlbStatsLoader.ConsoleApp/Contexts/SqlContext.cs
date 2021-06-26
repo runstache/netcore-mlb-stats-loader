ddab0a8e0d0af31d518cff5685b1fe5e56ea07a4
Larry Shields <larry.shields@gmail.com>
2021-06-26 16:25:10 -0400

    Add Sql Server DB Context

---
+++ src/MlbStatsLoader.ConsoleApp/Contexts/SqlContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MlbStatsLoader.ConsoleApp.Configurations;
+using MlbStatsLoader.ConsoleApp.DataObjects;
+
+namespace MlbStatsLoader.ConsoleApp.Contexts
+{
+    public class SqlContext : DbContext
+    {
+        private readonly string connString;
+        public SqlContext(string connectionString)
+        {
+            this.connString = connectionString;
+        }
+
+        public SqlContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<PlayerStat> PlayerStats { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<PitcherStat> PitchingStats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured && !string.IsNullOrEmpty(connString))
+            {                
+                optionsBuilder.UseSqlServer(connString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new PlayerConfiguration());
+            modelBuilder.ApplyConfiguration(new TeamConfiguration());
+            modelBuilder.ApplyConfiguration(new PlayerStatConfiguration());
+            modelBuilder.ApplyConfiguration(new PitcherStatConfiguration());
+        }
+    }
+}
